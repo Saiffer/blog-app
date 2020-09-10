87587ee493eb1366beece95b659874414298a199
Saiffer <32097584+Saiffer@users.noreply.github.com>
2020-09-10 14:26:56 -0400

    Finished backend, comments and upvotes. Ready for deployment

---
+++ client/src/components/AddCommentForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const AddCommentForm = ({ articleName, setArticleInfo }) => {
+  const [username, setUserName] = useState("");
+  const [commentText, setCommentText] = useState("");
+
+  const addComment = async () => {
+    const result = fetch(`/api/articles/${articleName}/addcomment`, {
+      method: "POST",
+      body: JSON.stringify({ username, text: commentText }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const body = await (await result).json();
+    setArticleInfo(body);
+    setUserName("");
+    setCommentText("");
+  };
+
+  return (
+    <div id='add-comment-form'>
+      <h3>Add a Comment</h3>
+      <label>
+        Name
+        <input
+          type='text'
+          value={username}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+      </label>
+      <label>
+        Comment
+        <textarea
+          rows='4'
+          cols='50'
+          value={commentText}
+          onChange={(event) => setCommentText(event.target.value)}
+        />
+      </label>
+      <button onClick={() => addComment()}>Add Comment</button>
+    </div>
+  );
+};
+
+export default AddCommentForm;
--- client/src/pages/ArticlePage.js
+++ client/src/pages/ArticlePage.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import articleContent from "./article-content";
 import ArticlesList from "../components/ArticlesList";
 import NotFoundPage from "./NotFoundPage";
+import CommentsList from "../components/CommentsList";
+import UpvoteSection from "../components/UpvoteSection";
+import AddCommentForm from "../components/AddCommentForm";
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articleContent.find((article) => article.name === name);
+
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`);
+      const body = await result.json();
+      console.log(body);
+      setArticleInfo(body);
+    };
+    fetchData();
+  }, [name]);
 
   if (!article) {
     return <NotFoundPage />;
@@ -18,9 +33,16 @@ const ArticlePage = ({ match }) => {
   return (
     <div>
       <h1>{article.title}</h1>
+      <UpvoteSection
+        articleName={name}
+        upvotes={articleInfo.upvotes}
+        setArticleInfo={setArticleInfo}
+      />
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <CommentsList comments={articleInfo.comments} />
+      <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
       <ArticlesList articles={otherArticles} />
     </div>
   );
